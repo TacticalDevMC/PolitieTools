201ed67ae17fe0b2d27f781ccc4f5ab9170e5c88
HeroDevelopment <32772380+joran1234@users.noreply.github.com>
2019-03-02 00:03:24 +0100

    Update HelpSubCommand
    If player has not the permission help that is standing on config, then is the CROSS icon (❌) red
    End the player have the permission then is de CHECKMARK icon (✅) green, then can you see which command you can do
    
    Easy is that anyway?

--- src/main/java/me/joran/politietools/commands/subcommands/HelpSubCommand.java
+++ src/main/java/me/joran/politietools/commands/subcommands/HelpSubCommand.java
@@ -8,15 +8,15 @@ import org.bukkit.entity.Player;
 
 public class HelpSubCommand {
 
-    private String perm = FileManager.get("config.yml").getString("permissions.help");
+    private String permission = FileManager.get("config.yml").getString("permissions.help");
 
     public void onCommand(CommandSender sender, String label, String[] args) {
         Player player = (Player) sender;
 
         if (args.length == 0) {
-            if (player.hasPermission(perm)) player.sendMessage(ChatUtils.format("&a" + Icon.CHECKMARK + " /112, 100, 101, 102 <bericht>"));
-            if (player.hasPermission(perm)) player.sendMessage(ChatUtils.format("&a" + Icon.CHECKMARK + " /politietools <help>"));
-            if (!player.hasPermission(perm)) player.sendMessage(ChatUtils.format("&c" + Icon.CROSS + " /politietools <help>"));
+            if (player.hasPermission(permission)) player.sendMessage(ChatUtils.format("&a" + Icon.CHECKMARK + " /112, 100, 101, 102 <bericht>"));
+            if (player.hasPermission(permission)) player.sendMessage(ChatUtils.format("&a" + Icon.CHECKMARK + " /politietools <help>"));
+            if (!player.hasPermission(permission)) player.sendMessage(ChatUtils.format("&c" + Icon.CROSS + " /politietools <help>"));
         } else {
             if (args[1].equalsIgnoreCase("112")) {
                 player.sendMessage(ChatUtils.format("&7&m------------------"));
